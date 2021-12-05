54114ace7d3e8937e1065a4ceeb3e22e07fe91d9
khayym <xeyyam.kerimov@gmail.com>
2021-12-06 02:11:00 +0400

    new final vercion | v1.2

--- games/millionare/app/app.js
+++ games/millionare/app/app.js
@@ -5,15 +5,17 @@ let answers = [];
 let correctAnswer ;
 let nextQuestion = 0;
 let moneyPramid_count = 0;
-
-
+let resSecond =[];
+let intervalId;
+let currentTime;
+let interval;
 
 class DomElements{
     static moneyPramid = $('#money-pramid')
     static answerBlock = $('.ans')
     static questionDiv = document.getElementById('questionDiv');
     static fiftyButton  = document.querySelector('#fifty');
-    
+    static timer = document.querySelector('.timer');
     // Sounds
     static letsPlay = document.querySelector('#lets-play');
     static easy = document.querySelector('#easy');
@@ -36,11 +38,6 @@ class Gaming extends DomElements{
         answers = this.jsonData [randomNum].answers
         correctAnswer = this.jsonData [randomNum].correct_answers
 
-        console.log(dataList);
-        console.log(data_Questions);
-        console.log(answers);
-        console.log(correctAnswer);
-
         // Question Block
         DomElements.answerBlock[0].innerHTML = answers[0]
         DomElements.answerBlock[1].innerHTML = answers[1]
@@ -50,11 +47,32 @@ class Gaming extends DomElements{
     }   
 
     
+    timerFunction = () => {
+        currentTime = new Date().getTime();
+        intervalId = setInterval(() => {
+           interval = Math.floor(
+                (41000 + currentTime - new Date().getTime()) / 1000
+            );
+            DomElements.timer.textContent = interval;
+            if (interval === 0) {
+                this.loserEnd()
+                clearInterval(intervalId);
+            }
+            return interval;
+        }, 100);
+    }
+
+    offTimer = () => {
+        setTimeout(() => {
+            clearInterval(intervalId);
+        },200)
+    }
+
     
     loserEnd(){
         $(`.ca_${correctAnswer}`).css('background','green')
         Sounds.wrongAnswerSound();
-        setTimeout(()=>document.querySelector('[data-target="#exampleModal"]').click(),1500)
+        setTimeout(()=>document.querySelector('[data-target="#exampleModal"]').click(),1500); 
     }
 
 
@@ -65,6 +83,7 @@ class Gaming extends DomElements{
                 alert('You Winnnnn');
                 window.location.reload();
             }else{
+               
                 $(`#mp-${moneyPramid_count}`).css(
                     {
                     "border": "1px solid #fff", 
@@ -73,6 +92,7 @@ class Gaming extends DomElements{
                     "background":"linear-gradient(27deg, rgba(135,126,56,1) 12%, rgba(138,138,51,1) 39%, rgba(203,184,34,1) 99%)"
                 });
                 this.dataConvert();
+                this.timerFunction();
                 $(`#${e}`).css('background','#021442')
             }
         }
@@ -117,7 +137,6 @@ class Jokers extends Gaming{
         $('#call-modal-p').html(`Einstein cavabın <b> <i> ${answers[correctAnswer]} </i></b> olduğunu düşünür`);
         setTimeout(()=>document.querySelector('[data-target="#callFriendModal"]').click(),200)
     }
-    
 }
 
 
@@ -155,41 +174,36 @@ class Sounds extends Gaming{
 fetch('/gaming-portfolio/games/millionare/app/data.json')
 .then(response => response.json())
 .then(data => {
-    
-    let mainClass = new Gaming(data)
 
+    let mainClass = new Gaming(data)
     mainClass.dataConvert();
     return mainClass
 })
 .then(allData => {
-    
     const sound =  new Sounds();
     const jokers = new Jokers();
+    allData.timerFunction();
     sound.startSound();
     
     $(document).on('click','#fifty',()=> jokers.fifty());
-
     $(document).on('click','#callJokerBTN',()=> jokers.callFriend());
-
- 
 
     $(document).on('click','.ans',(e)=>{
         $(`#${e.target.id}`).css('background','#037888')
         jokers.fiftyDisable()
 
+        setTimeout(()=>allData.offTimer(),300)
         setTimeout(()=>{   
-            const buttonVal = e.target.value;
+            const buttonVal = e.target.value;             
             sound.stop_startSound();
             sound.correctAnswer()
             allData.checkUserOrder(buttonVal,e.target.id);
-        },1000)
+        },3000)
     })
 })
 
 
-function restartBtt(){
-    window.location.reload();
-}
+function restartBtt(){ window.location.reload();}
 
 
 
